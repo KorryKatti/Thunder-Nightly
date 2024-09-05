86ad2119cc6754620410caaa3ded0a1536f32bbe
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-09-05 22:36:32 +0530

    more updates
    my hand pains due to the small table and using keyboard and laptop with its stand

--- backend.py
+++ backend.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import json
 import os
 app = Flask(__name__)
 
@@ -9,6 +11,32 @@ app = Flask(__name__)
 if not os.path.exists('local'):
     os.makedirs('local')
 
+#check if settings.json exists or not if it does not , create it and add some default values
+if not os.path.exists('local/settings.json'):
+    with open('local/settings.json', 'w') as f:
+        f.write('{\n"server": "http://127.0.0.1:4325"}')
+        f.close()
+
+
+with open('local/settings.json') as f:
+    settings = json.load(f)
+    server = settings['server']
+    f.close()
+
+
+response = requests.get(server+'/ping')
+
+
+
+print("Running on server : ",server)
+print('''
+Hello welcome to Thunder. Hang on while we ping the server to check if it is up and running
+''')
+print(response.text)
+print("Seems like it worked. Starting app now. Initializing backend")
+print("                                     ")
+ 
+#check if server is up and running at server/ping
 
 
 
