23c18a5f11352023acf3ac690cedd06739f72cd1
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-11-12 09:34:57 +0530

    done some app page work
    thank god for google and chatgpt , frontend is hard

--- backend.py
+++ backend.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import os
 import json
+import requests
 import webbrowser
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 USER_DATA_LOADED = False
@@ -44,6 +46,36 @@ def homepage():
 def store():
     return render_template("store.html",**data_hell_yeah)
 
+@app.route('/app/<app_id>')
+def fetch_app_page(app_id):
+    # Construct the URL of the external page
+    url = f'https://korrykatti.github.io/thapps/apps/{app_id}.html'
+    
+    # Fetch the page content
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Extract elements based on their `id`
+        app_data = {
+            "app_id": soup.find(id="appId").text if soup.find(id="appId") else "N/A",
+            "app_name": soup.find(id="appName").text if soup.find(id="appName") else "N/A",
+            "icon_url": soup.find(id="iconUrl").text if soup.find(id="iconUrl") else "",
+            "version": soup.find(id="version").text if soup.find(id="version") else "N/A",
+            "repo_url": soup.find(id="repoUrl").text if soup.find(id="repoUrl") else "",
+            "main_file": soup.find(id="mainFile").text if soup.find(id="mainFile") else "N/A",
+            "description": soup.find(id="description").text if soup.find(id="description") else "No description available."
+        }
+        
+        # Render the template with the extracted data
+        return render_template('app_page.html', **app_data)
+    else:
+        # If there is an issue fetching the page, return a 404 error
+        return "Page not found", 404
+    
+    
 if __name__ == "__main__":
     webbrowser.open("http://127.0.0.1:6969")
     app.run(debug=True, port=6969)
